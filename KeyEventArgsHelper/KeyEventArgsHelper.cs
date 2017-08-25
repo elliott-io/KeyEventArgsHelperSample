0be34bce2a65da9e4a8670cdb27cca25762b90e3
Justin Elliott <meraki.club@gmail.com>
2017-08-24 22:22:09 -0400

    Added files.

---
+++ KeyEventArgsHelper/KeyEventArgsHelper.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Input;
+
+namespace KeyEventArgsHelperSample
+{
+    public class KeyEventArgsHelper
+    {
+        public static bool ModifierKey(KeyEventArgs e)
+        {
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.None)
+                return false;
+            else
+                return true;
+        }
+        public static bool SystemKey(KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                //case Key.LeftAlt:
+                //    return true;
+                //case Key.RightAlt:
+                //    return true;
+                //case Key.LeftCtrl:
+                //    return true;
+                //case Key.RightCtrl:
+                //    return true;
+                //case Key.LeftShift:
+                //    return true;
+                //case Key.RightShift:
+                //    return true;
+                //case Key.LWin:
+                //    return true;
+                //case Key.RWin:
+                //    return true;
+                case Key.Tab:
+                    return true;
+                case Key.CapsLock:
+                    return true;
+                case Key.Up:
+                    return true;
+                case Key.Down:
+                    return true;
+                case Key.Left:
+                    return true;
+                case Key.Right:
+                    return true;
+                case Key.Home:
+                    return true;
+                case Key.End:
+                    return true;
+                case Key.PageUp:
+                    return true;
+                case Key.PageDown:
+                    return true;
+                case Key.Insert:
+                    return true;
+                case Key.Print:
+                    return true;
+                case Key.PrintScreen:
+                    return true;
+                case Key.Escape:
+                    return true;
+                case Key.Delete:
+                    return true;
+                case Key.Back:
+                    return true;
+                case Key.F1:
+                    return true;
+                case Key.F10:
+                    return true;
+                case Key.F11:
+                    return true;
+                case Key.F12:
+                    return true;
+                case Key.F13:
+                    return true;
+                case Key.F14:
+                    return true;
+                case Key.F15:
+                    return true;
+                case Key.F16:
+                    return true;
+                case Key.F17:
+                    return true;
+                case Key.F18:
+                    return true;
+                case Key.F19:
+                    return true;
+                case Key.F20:
+                    return true;
+                case Key.F2:
+                    return true;
+                case Key.F3:
+                    return true;
+                case Key.F4:
+                    return true;
+                case Key.F5:
+                    return true;
+                case Key.F6:
+                    return true;
+                case Key.F7:
+                    return true;
+                case Key.F8:
+                    return true;
+                case Key.F9:
+                    return true;
+                case Key.NumLock:
+                    return true;
+                case Key.System:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ KeyEventArgsHelper/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyEventArgsHelperSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            maxChar.Text = "20";
+            input.MaxLength = 20;
+
+            maxChar.PreviewTextInput += MaxChar_PreviewTextInput;
+            maxChar.TextChanged += MaxChar_TextChanged;
+
+            input.TextChanged += Input_TextChanged;
+            input.PreviewKeyDown += Input_PreviewKeyDown;
+
+            clearButton.Click += ClearButton_Click;
+
+            input.Focus();
+        }
+
+        private void Input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            output.Clear();
+
+            if (KeyEventArgsHelper.ModifierKey(e))
+            {
+                output.Text = "Modifier Key: " + Keyboard.Modifiers.ToString();
+            }
+            if (KeyEventArgsHelper.SystemKey(e))
+            {
+                if (output.Text != string.Empty)
+                    output.Text += " + ";
+
+                output.Text += "System Key: " + e.Key;
+            }
+            else
+            {
+                if (e.Key != Key.LeftAlt && e.Key != Key.RightAlt &&
+                    e.Key != Key.LeftCtrl && e.Key != Key.RightCtrl &&
+                    e.Key != Key.LeftShift && e.Key != Key.RightShift &&
+                    e.Key != Key.LWin && e.Key != Key.RWin)
+                {
+                    if (output.Text != string.Empty)
+                        output.Text += " + ";
+
+                    output.Text += e.Key.ToString();
+
+                    if (Keyboard.Modifiers.Equals(ModifierKeys.Control) && e.Key != Key.V)
+                    {
+                        // do nothing
+                    }
+                    else if (CheckMax())
+                        output.Text += " || Beep boop. Max characters reached.";
+                }
+            }
+        }
+        public bool CheckMax()
+        {
+            if (input.Text.Length == input.MaxLength)
+                return true;
+            else
+                return false;
+        }
+
+        private void Input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            counter.Text = input.Text.Length + " of " + maxChar.Text;
+        }
+
+        private void MaxChar_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+                e.Handled = true;
+        }
+
+        private void MaxChar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            input.MaxLength = Convert.ToInt32(maxChar.Text);
+            counter.Text = input.Text.Length + " of " + maxChar.Text;
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            input.Clear();
+            maxChar.Text = "20";
+            input.MaxLength = 20;
+            output.Clear();
+        }
+    }
+}
